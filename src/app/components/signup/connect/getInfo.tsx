dd0529b0c53564059e1acb4135f1c42f98aa457b
annsinkevich47 <anneta.sinkevich@mail.ru>
2024-05-13 21:30:36 +0300

    feat: add values of input to request

--- src/app/components/signup/connect/getInfo.tsx
+++ src/app/components/signup/connect/getInfo.tsx
@@ -1,14 +1,29 @@
+import states from '../states';
 import createApiRoot from './client';
 
-const getEndpoints = (name: string, surname: string, mail: string, pass: string) => {
+const getEndpoints = (allInputValues: Array<string>) => {
   return createApiRoot()
     .customers()
     .post({
       body: {
-        firstName: name,
-        lastName: surname,
-        email: mail,
-        password: pass,
+        firstName: allInputValues[0],
+        lastName: allInputValues[1],
+        dateOfBirth: allInputValues[2],
+        addresses: [
+          {
+            id: 'address1',
+            firstName: allInputValues[0],
+            lastName: allInputValues[1],
+            streetName: allInputValues[5],
+            postalCode: allInputValues[6],
+            city: allInputValues[4],
+            country: states[allInputValues[3]],
+            phone: allInputValues[7],
+            email: allInputValues[8],
+          },
+        ],
+        email: allInputValues[8],
+        password: allInputValues[9],
       },
     })
     .execute()
--- src/app/components/signup/submitBtn.tsx
+++ src/app/components/signup/submitBtn.tsx
@@ -2,8 +2,7 @@ import getEndpoints from './connect/getInfo';
 
 const pressSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, storage: Array<string>) => {
   e.preventDefault();
-  console.log(storage);
-  getEndpoints(storage[0], storage[1], storage[8], storage[9]);
+  getEndpoints(storage);
 };
 
 export default pressSubmit;
