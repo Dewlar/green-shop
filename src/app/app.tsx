cabfe245f848d429fe1880e65b4e34ada93dee1f
letanatol <letanatol@gmail.com>
2024-05-16 19:08:31 +0300

    feat: add AppContext

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/about';
@@ -10,22 +10,27 @@ import Catalog from './pages/catalog';
 import Home from './pages/home';
 import Signup from './pages/signup';
 import UserProfile from './pages/user-profile';
+import { AppContext, initialValues } from './components/AppContext';
 
 const App = () => {
+  const [auth, setAuth] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Product />} path="product" />
-        <Route element={<Cart />} path="cart" />
-        <Route element={<Catalog />} path="catalog" />
-        <Route element={<Home />} path="/" />
-        <Route element={<Login />} path="login" />
-        <Route element={<Signup />} path="signup" />
-        <Route element={<About />} path="about" />
-        <Route element={<UserProfile />} path="profile" />
-        <Route element={<Page404 />} path="*" />
-      </Routes>
-    </BrowserRouter>
+    <AppContext.Provider value={{ ...initialValues, isAuth: auth, setIsAuth: setAuth }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Product />} path="product" />
+          <Route element={<Cart />} path="cart" />
+          <Route element={<Catalog />} path="catalog" />
+          <Route element={<Home />} path="/" />
+          <Route element={<Login />} path="login" />
+          <Route element={<Signup />} path="signup" />
+          <Route element={<About />} path="about" />
+          <Route element={<UserProfile />} path="profile" />
+          <Route element={<Page404 />} path="*" />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
