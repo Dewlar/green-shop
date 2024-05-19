c607594448a3623be2db21d3442a883978bd5b28
letanatol <letanatol@gmail.com>
2024-05-19 22:15:35 +0300

    feat: add header to login page

--- src/app/pages/login.tsx
+++ src/app/pages/login.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import LoginForm from '../layout/login-form/login-form';
+import HeaderWidthMobile from '../components/header/header-width-mobile';
 
 const Login = () => {
   return (
     <div>
+      <HeaderWidthMobile></HeaderWidthMobile>
       <LoginForm></LoginForm>
     </div>
   );
