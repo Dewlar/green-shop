1f2bca4742c6bf01a5d75b0bb81b4aadfd1e3f16
letanatol <100472058+letanatol@users.noreply.github.com>
2024-05-20 21:42:29 +0300

    fix: change layout for login page (#16)

--- src/app/layout/login-form/login-form.tsx
+++ src/app/layout/login-form/login-form.tsx
@@ -1,7 +1,7 @@
 import React, { MouseEvent, useState } from 'react';
 import { toast } from 'react-toastify';
 import { HttpErrorType } from '@commercetools/sdk-client-v2';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import CustomerController from '../../api/CustomerController';
 import { IAuth, useStateContext } from '../../state/state-context';
 import { LocalStorageKeysEnum, storageSet } from '../../api/helpers';
@@ -133,7 +133,12 @@ const LoginForm = () => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className="mx-auto h-10 w-auto" src="./assets/logo/logo.png" alt="logo" />
+        <div className="flex justify-center w-full items-center">
+          <Link to="/">
+            <span className="sr-only">Green shop</span>
+            <img className="h-8 w-auto" src="./assets/logo/logo.png" alt="logo" />
+          </Link>
+        </div>
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
@@ -213,6 +218,12 @@ const LoginForm = () => {
             </button>
           </div>
         </form>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not have an account?{' '}
+          <Link to="/signup" className="text-sm font-medium text-indigo-600 hover:text-gray-700">
+            Sign up
+          </Link>
+        </p>
       </div>
     </div>
   );
