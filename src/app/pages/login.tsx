a9d65b35056fad20b11aafaa459297d6a9e789ed
letanatol <letanatol@gmail.com>
2024-05-20 14:52:00 +0300

    fix: change layout for login page

--- src/app/pages/login.tsx
+++ src/app/pages/login.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import LoginForm from '../layout/login-form/login-form';
-import HeaderWidthMobile from '../components/header/header-width-mobile';
 
 const Login = () => {
   return (
     <div>
-      <HeaderWidthMobile></HeaderWidthMobile>
       <LoginForm></LoginForm>
     </div>
   );
