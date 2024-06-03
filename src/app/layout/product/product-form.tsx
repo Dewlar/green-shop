aef4441b5a32b4fadefa81fef7f6ea0f8bebe51a
annsinkevich47 <anneta.sinkevich@mail.ru>
2024-06-03 19:56:02 +0300

    fix: logo and path to product page

--- src/app/layout/product/product-form.tsx
+++ src/app/layout/product/product-form.tsx
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Product } from '@commercetools/platform-sdk';
+import { useParams } from 'react-router-dom';
 import ProductMain from '../../components/product/mainLayout';
 import HeaderWidthMobile from '../../components/header/header-width-mobile';
 import Footer from '../../components/footer/footer';
 import { getOneProduct, getProductData } from '../../components/product/getProduct/getDataProduct';
 
 const ProductForm = () => {
+  const { id } = useParams();
   const [productStorage, setProduct] = useState({} as Product);
+  const depth = '2';
 
   useEffect(() => {
     async function response() {
+      if (!id) return;
       await getProductData();
-      await getOneProduct('76f95e70-0cd4-4b42-94d5-de5f2c288b55').then(({ body }) => {
+      await getOneProduct(id).then(({ body }) => {
         setProduct(body);
       });
     }
     response();
-  }, []);
+  }, [id]);
 
   return (
     <div>
-      <HeaderWidthMobile></HeaderWidthMobile>
+      <HeaderWidthMobile depth={depth}></HeaderWidthMobile>
       <ProductMain {...productStorage}></ProductMain>
       <Footer></Footer>
     </div>
