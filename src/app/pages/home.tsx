8ad0e9a85e10f240e7789bbddbdc89cf5a8ff2a5
Andrei Markouski <insawm@gmail.com>
2024-06-03 22:48:57 +0300

    fix: clear input search value

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Footer from '../components/footer/footer';
 import HeaderWidthMobile from '../components/header/header-width-mobile';
 import Policies from '../components/main-page/policies';
@@ -12,19 +11,6 @@ const Home = () => {
   return (
     <div className="bg-white">
       {/* Header with mobile burger menu */}
-      <div className="bg-green-50">
-        <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-          <div className="text-sm text-gray-500">Cross check links:</div>
-          <div className="flex items-center space-x-6">
-            <Link to="/login" className="text-sm font-medium text-gray-500 hover:text-gray-700">
-              Sign in
-            </Link>
-            <Link to="/signup" className="text-sm font-medium text-gray-500 hover:text-gray-700">
-              Sign up
-            </Link>
-          </div>
-        </div>
-      </div>
       <HeaderWidthMobile></HeaderWidthMobile>
 
       <main>
