df2de042935e3b59885f4e435d7f0058d146be86
Andrei Markouski <insawm@gmail.com>
2024-06-11 23:36:44 +0300

    refactor: about page styles

--- src/app/layout/about/about-layout.tsx
+++ src/app/layout/about/about-layout.tsx
@@ -10,13 +10,13 @@ const AboutLayout = () => {
             className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-green-600/10 ring-1 ring-green-100 sm:-mr-80 lg:-mr-96"
             aria-hidden="true"
           />
-          <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+          <div className="mx-auto max-w-7xl px-6 py-32 sm:py-16 lg:px-8">
+            <div className="mx-auto lg:mx-0 grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
               <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:col-span-2 xl:col-auto">
                 We’re a <span className="text-green-500">R.S.</span>
                 <span className="text-gray-500">Survivor</span> team.
               </h1>
-              <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+              <div className="mt-6 max-w-2xl lg:mt-0 xl:col-end-1 xl:row-start-1">
                 <p className="text-xl leading-8 text-gray-600 text-justify">
                   We present a project created as part of the RSSchool coursework. The project is a web application for
                   an online store. The project was created using the React library and uses the eCommerce tools API.
@@ -28,7 +28,7 @@ const AboutLayout = () => {
               <img
                 src="./assets/about/about.jpg"
                 alt=""
-                className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+                className="mt-10 aspect-[16/9] w-full max-w-none rounded-2xl object-cover sm:mt-16 lg:aspect-[6/5] lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
               />
             </div>
           </div>
@@ -36,9 +36,9 @@ const AboutLayout = () => {
         </div>
 
         {/* Content section */}
-        <div className="my-32 overflow-hidden sm:mt-40">
-          <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
-            <div className="mx-auto max-w-2xl lg:mx-0 grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-2 xl:gap-x-8">
+        <div className="my-24 overflow-hidden sm:mt-24">
+          <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8 justify-center">
+            <div className="mx-auto max-w-2xl lg:mx-0 grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-2 xl:gap-x-0">
               <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:col-span-2">
                 Our team.
               </h1>
@@ -56,17 +56,17 @@ const AboutLayout = () => {
               <img
                 src="./assets/about/about.jpg"
                 alt=""
-                className="mt-10 justify-self-end aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:mt-36"
+                className="mt-10 justify-self-end aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-lg xl:mt-36"
               />
               <img
                 src="./assets/about/photo.jpg"
                 alt=""
-                className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:-mt-32 lg:max-w-none xl:-mt-40"
+                className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:-mt-32 lg:max-w-lg xl:-mt-40"
               />
               <img
                 src="./assets/about/about.jpg"
                 alt=""
-                className="mt-10 justify-self-end aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:mt-36"
+                className="mt-10 justify-self-end aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-lg xl:mt-36"
               />
             </div>
           </div>
