e4e3a144d1e5d32e731d803f9d29ac99a94e9a0c
Andrei Markouski <insawm@gmail.com>
2024-06-19 17:40:01 +0300

    fix: disable next button when no result found

--- src/app/layout/catalog-form/catalog-pagination.tsx
+++ src/app/layout/catalog-form/catalog-pagination.tsx
@@ -43,6 +43,7 @@ const CatalogPagination: FC<Props> = ({ pageCounter, setPageCounter }) => {
 
   const handleNext = () => {
     if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    console.log(currentPage);
   };
 
   return (
@@ -67,7 +68,7 @@ const CatalogPagination: FC<Props> = ({ pageCounter, setPageCounter }) => {
         </div>
         <button
           onClick={handleNext}
-          disabled={currentPage === totalPages}
+          disabled={currentPage === totalPages || currentPage === 1}
           className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 disabled:bg-gray-300"
         >
           Next
@@ -121,7 +122,7 @@ const CatalogPagination: FC<Props> = ({ pageCounter, setPageCounter }) => {
             })}
             <button
               onClick={handleNext}
-              disabled={currentPage === totalPages}
+              disabled={currentPage === totalPages || currentPage === 1}
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:bg-gray-300"
             >
               <span className="sr-only">Next</span>
