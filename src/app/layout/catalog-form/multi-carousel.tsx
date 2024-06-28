00c55ff7b4b58983f8fd085cbcd275a0c15e6b90
Andrei Markouski <insawm@gmail.com>
2024-06-28 20:55:36 +0300

    feat: add carousel to catalog page with sample images

---
+++ src/app/layout/catalog-form/multi-carousel.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const MultiCarousel = () => {
+  return (
+    <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlay
+      autoPlaySpeed={10000}
+      centerMode={false}
+      className=""
+      containerClass="container-with-dots"
+      dotListClass=""
+      draggable
+      focusOnSelect={false}
+      infinite
+      itemClass="w-full h-96 mx-auto flex"
+      keyBoardControl
+      minimumTouchDrag={80}
+      pauseOnHover
+      renderArrowsWhenDisabled={false}
+      renderButtonGroupOutside={false}
+      renderDotsOutside={false}
+      responsive={{
+        desktop: {
+          breakpoint: {
+            max: 3000,
+            min: 1024,
+          },
+          items: 1,
+          partialVisibilityGutter: 10,
+        },
+        mobile: {
+          breakpoint: {
+            max: 464,
+            min: 0,
+          },
+          items: 1,
+          partialVisibilityGutter: 10,
+        },
+        tablet: {
+          breakpoint: {
+            max: 1024,
+            min: 464,
+          },
+          items: 1,
+          partialVisibilityGutter: 10,
+        },
+      }}
+      rewind={false}
+      rewindWithAnimation={false}
+      rtl={false}
+      shouldResetAutoplay
+      showDots={true}
+      sliderClass=""
+      slidesToSlide={1}
+      swipeable
+    >
+      <div className="w-full h-full mx-auto block">
+        <img
+          className="w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+      </div>
+      <div className="w-full h-full mx-auto block">
+        <img
+          className="w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+      </div>
+      <div className="w-full h-full mx-auto block">
+        <img
+          className="w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
+      </div>
+    </Carousel>
+  );
+};
+
+export default MultiCarousel;
